18002740ba7d480046469f71e449e8aa38f1f1d3
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-05-31 13:28:02 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,33 @@
-#include <iostream>
 #include <assert.h>
+#define NUMBER_OF_COLORS 5
+
+int fetchMajorColorFromPairNumber(const int pairNumber) {
+    const int zeroBasedPairNumber = pairNumber - 1;
+    const int majorColorNumber = zeroBasedPairNumber / NUMBER_OF_COLORS;
+    return majorColorNumber;
+}
+
+int fetchMinorColorFromPairNumber(const int pairNumber) {
+    const int zeroBasedPairNumber = pairNumber - 1;
+    const int minorColorNumber = zeroBasedPairNumber % NUMBER_OF_COLORS;
+    return minorColorNumber;
+}
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
+    int i = 0, j = 0, pairNumber, acceptedMajorColor, acceptedMinorColor;
+    
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            pairNumber = i * 5 + j;
+            acceptedMajorColor = fetchMajorColorFromPairNumber(pairNumber);
+            acceptedMinorColor = fetchMinorColorFromPairNumber(pairNumber);
+
+            assert(majorColor[acceptedMajorColor] == majorColor[i]);
+            assert(minorColor[acceptedMinorColor] == minorColor[j]);
+            
+            std::cout << pairNumber << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
         }
     }
     return i * j;
