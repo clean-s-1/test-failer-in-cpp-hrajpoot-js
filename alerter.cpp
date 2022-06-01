e54444e0326d20c4089f11b2052d672f86e5998f
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-01 22:40:57 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -20,6 +20,7 @@ void alertInCelcius(float farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
+        assert(alertFailureCount > 0);
     }
 }
 
