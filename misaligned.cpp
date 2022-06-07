d4febde833a1f31515a7056d02924b0e8e8bcb46
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-07 11:32:18 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include <assert.h>
+#include <string>
 #define NUMBER_OF_COLORS 5
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 int fetchMajorColorFromPairNumber(const int pairNumber) {
     const int zeroBasedPairNumber = pairNumber - 1;
     const int majorColorNumber = zeroBasedPairNumber / NUMBER_OF_COLORS;
-    return majorColorNumber;
+    return majorColorNumber;  
 }
 
 int fetchMinorColorFromPairNumber(const int pairNumber) {
@@ -13,21 +18,36 @@ int fetchMinorColorFromPairNumber(const int pairNumber) {
     return minorColorNumber;
 }
 
+std::string getFormatedColorPairString(int majorColorIndex, int minorColorIndex)
+{
+    const int pairNumber = majorColorIndex * 5 + minorColorIndex + 1;
+    std::string formatedColorPairString = std::to_string(pairNumber) + " | " + majorColor[majorColorIndex] + " | " + minorColor[minorColorIndex];
+    return formatedColorPairString;
+}
+
+void printFormatedColorPair(std::string colorPairString)
+{
+    std::cout << colorPairString << std::endl;
+}
+
+void validateColorPairString(int majorColorIndex, int minorColorIndex, const std::string colorPairString)
+{
+    const int colorPairNumber = majorColorIndex * NUMBER_OF_COLORS + minorColorIndex + 1;
+    int acceptedMajorColor = fetchMajorColorFromPairNumber(colorPairNumber);
+    int acceptedMinorColor = fetchMinorColorFromPairNumber(colorPairNumber);
+
+    std::string acceptedColorPairString = std::to_string(colorPairNumber) + " | " + majorColor[acceptedMajorColor] + " | " + minorColor[acceptedMinorColor];
+    assert(colorPairString == acceptedColorPairString);
+}
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0, pairNumber, acceptedMajorColor, acceptedMinorColor;
-    
+    int i = 0, j = 0;
+
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            pairNumber = i * 5 + j;
-            acceptedMajorColor = fetchMajorColorFromPairNumber(pairNumber);
-            acceptedMinorColor = fetchMinorColorFromPairNumber(pairNumber);
-
-            assert(majorColor[acceptedMajorColor] == majorColor[i]);
-            assert(minorColor[acceptedMinorColor] == minorColor[j]);
-            
-            std::cout << pairNumber << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            std::string formatedColorPairString = getFormatedColorPairString(i, j);
+            validateColorPairString(i, j, formatedColorPairString);
+            printFormatedColorPair(formatedColorPairString);
         }
     }
     return i * j;
@@ -36,6 +56,6 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    std::cout << "All is well (maybe!)\n";
+    std::cout << "All is well \n";
     return 0;
 }
